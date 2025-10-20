6b71c0966fd26151f1ca83fa393b55048bccad4d
Charles-de-Chabot <chabot0302@gmail.com>
2025-10-20 10:16:42 +0200

    mise en place du js pour attendre le chargement du dom

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ console.log("Hello, World!");
         <div class="cardBack"></div>
         <div class="cardImage" style="background-image:url(img/12.png)"></div>
       </div> */
+
+      function handlerDomContentLoaded() {
+        //Le code du jeu
+        console.log("Le jeu peut commencer !");
+        
+      }
+// Mise en place de l'écouteur d'événement pour ne lancer le code qu'une fois le DOM entièrement chargé par le navigateur.
+document.addEventListener("DOMContentLoaded", handlerDomContentLoaded);      
