23d5e4a97a81a27da81ecda0e1b344f07ec6041f
Charles-de-Chabot <chabot0302@gmail.com>
2025-10-20 09:10:59 +0200

    preparation du plateau de jeu en qttente du javascript

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("Hello, World!");
+/* <div class="card">
+        <div class="cardBack"></div>
+        <div class="cardImage" style="background-image:url(img/12.png)"></div>
+      </div> */
